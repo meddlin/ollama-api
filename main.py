87c96746d351646f3389337857de2420643ce72f
meddlin <drushing.dev@gmail.com>
2024-08-28 00:08:04 -0400

    wrapped it all in FastAPI

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import requests
 import json
+from typing import Union
+from fastapi import FastAPI
 
-url = "http://localhost:11434/api/chat"
+app = FastAPI()
 
-def llama3(prompt):
-    data = {
-        "model": "llama3.1",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "stream": False,
-    }
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-    headers = {
-        "Content-Type": "application/json"
-    }
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()["message"]["content"]
+@app.get("/ollama/{prompt}")
+def ollama_query(prompt: str):
+    url = "http://localhost:11434/api/chat"
 
-response = llama3("who wrote the book godfather")
-print(response)
+    def llama3(prompt):
+        data = {
+            "model": "llama3.1",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            "stream": False,
+        }
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        return response.json()["message"]["content"]
+
+    response = llama3(prompt)
+    print(response)
+    return {"response": response }
