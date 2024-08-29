7c92f88f67a3f5e78fee0de303e122975419205e
meddlin <drushing.dev@gmail.com>
2024-08-28 22:35:40 -0400

    Call Ollama from an API. And docs on fun stuff

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ An API wrapper for Ollama
 
 ## Getting Started
 
+## Doing More
+
 1. Install Ollama
 
 [https://ollama.com/](https://ollama.com/)
@@ -34,4 +36,23 @@ python -m venv .
 
 # activate environment
 source ./bin/activate
+```
+
+## Want more?
+
+Local alternative to GitHub Copilot
+
+See here for using models from Ollama to create a self-hosted version of GitHub Copilot
+
+[https://llama.meta.com/docs/integration-guides/meta-code-llama](https://llama.meta.com/docs/integration-guides/meta-code-llama)
+
+Doing this requires a VSCode extension, "Continue". This link shows an example config 
+file for choosing a locally available model.
+
+[https://gist.github.com/pythoninthegrass/9ec5d6e9e05b96272bb21d8a8ce2ca11](https://gist.github.com/pythoninthegrass/9ec5d6e9e05b96272bb21d8a8ce2ca11)
+
+Run this to check which ollama models are available:
+
+```bash
+ollama list
 ```
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
 import json
-from typing import Union
+from typing import Union, Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Prompt(BaseModel):
+    prompt_text: str
+
+    def __init__(self, prompt_text = ""):
+        self.prompt_text = prompt_text
 
 app = FastAPI()
 
@@ -9,9 +16,41 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/ollama")
+def ollama_request(prompt):
+    # p = Prompt()
+    # p.prompt_text = prompt
+    
+    url = "http://localhost:11434/api/chat"
+
+    def llama3(prompt):
+        data = {
+            "model": "llama3.1",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            "stream": False,
+        }
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        return response.json()["message"]["content"]
+
+    response = llama3(prompt)
+    print(response)
+
+    # p.response = response
+    response = {
+        'prompt': prompt,
+        'result': response
+    }
+    return response
 
 @app.get("/ollama/{prompt}")
 def ollama_query(prompt: str):
